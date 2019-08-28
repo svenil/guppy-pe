09f4860ea64bc53a7ae1b94682b983fb029c9a01
Sverker Nilsson <sn@sncs.se>
2019-08-28 17:30:22 +0200

    updated generated by

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ sets
 """,
           author="Sverker Nilsson",
           author_email="sn@sncs.se",
-          url="http://guppy-pe.sourceforge.net",
+          url="https://svenil.github.io/guppy-pe",
           license='MIT',
           packages=[
             "guppy",
